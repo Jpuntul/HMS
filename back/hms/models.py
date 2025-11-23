4b41d5b6e8a378c83d68cb990472eb7f6ee960ea
Jutipong Puntuleng <p.jutipong13@gmail.com>
2025-11-22 21:29:42 -0500

    feat: add missing database models for complete HMS integration

--- back/hms/models.py
+++ back/hms/models.py
@@ -104,3 +104,186 @@ class Facility(models.Model):
             return Person.objects.get(ssn=self.gmssn)
         except Person.DoesNotExist:
             return None
+
+
+class Residence(models.Model):
+    TYPE_CHOICES = [
+        ("apartment", "Apartment"),
+        ("condominium", "Condominium"),
+        ("semidetached house", "Semi-Detached House"),
+        ("house", "House"),
+    ]
+
+    res_id = models.AutoField(primary_key=True, db_column="ResID")
+    address = models.CharField(max_length=100, db_column="Address")
+    city = models.CharField(max_length=50, db_column="City")
+    province = models.CharField(max_length=25, db_column="Province")
+    postal_code = models.CharField(max_length=6, db_column="PostalCode")
+    no_of_bedrooms = models.IntegerField(
+        null=True, blank=True, db_column="NoOfBedrooms"
+    )
+    type = models.CharField(max_length=30, choices=TYPE_CHOICES, db_column="Type")
+
+    class Meta:
+        db_table = "Residences"
+        managed = False
+
+    def __str__(self):
+        return f"{self.address}, {self.city}"
+
+
+class InfectionType(models.Model):
+    type_id = models.AutoField(primary_key=True, db_column="TypeID")
+    type_name = models.CharField(max_length=50, unique=True, db_column="TypeName")
+
+    class Meta:
+        db_table = "InfectionTypes"
+        managed = False
+
+    def __str__(self):
+        return self.type_name
+
+
+class Infection(models.Model):
+    ssn = models.IntegerField(db_column="SSN")
+    date = models.DateField(db_column="Date")
+    type_id = models.IntegerField(db_column="TypeID")
+
+    class Meta:
+        db_table = "Infections"
+        managed = False
+        unique_together = [["ssn", "date", "type_id"]]
+
+    def __str__(self):
+        return f"Infection {self.type_id} - {self.date}"
+
+    @property
+    def person(self):
+        """Get the related person data"""
+        try:
+            return Person.objects.get(ssn=self.ssn)
+        except Person.DoesNotExist:
+            return None
+
+    @property
+    def infection_type(self):
+        """Get the infection type"""
+        try:
+            return InfectionType.objects.get(type_id=self.type_id)
+        except InfectionType.DoesNotExist:
+            return None
+
+
+class VaccineType(models.Model):
+    type_id = models.AutoField(primary_key=True, db_column="TypeID")
+    type_name = models.CharField(max_length=50, unique=True, db_column="TypeName")
+
+    class Meta:
+        db_table = "VaccineTypes"
+        managed = False
+
+    def __str__(self):
+        return self.type_name
+
+
+class Vaccination(models.Model):
+    ssn = models.IntegerField(db_column="SSN")
+    type_id = models.IntegerField(db_column="TypeID")
+    date = models.DateField(db_column="Date")
+    no_of_dose = models.IntegerField(null=True, blank=True, db_column="NoOfDose")
+    fid = models.IntegerField(null=True, blank=True, db_column="FID")
+
+    class Meta:
+        db_table = "Vaccinations"
+        managed = False
+        unique_together = [["ssn", "type_id", "date"]]
+
+    def __str__(self):
+        return f"Vaccination {self.type_id} - {self.date}"
+
+    @property
+    def person(self):
+        """Get the related person data"""
+        try:
+            return Person.objects.get(ssn=self.ssn)
+        except Person.DoesNotExist:
+            return None
+
+    @property
+    def vaccine_type(self):
+        """Get the vaccine type"""
+        try:
+            return VaccineType.objects.get(type_id=self.type_id)
+        except VaccineType.DoesNotExist:
+            return None
+
+    @property
+    def facility(self):
+        """Get the facility where vaccination was administered"""
+        try:
+            return Facility.objects.get(fid=self.fid)
+        except Facility.DoesNotExist:
+            return None
+
+
+class Employment(models.Model):
+    essn = models.IntegerField(db_column="ESSN")
+    fid = models.IntegerField(db_column="FID")
+    start_date = models.DateField(db_column="StartDate")
+    end_date = models.DateField(null=True, blank=True, db_column="EndDate")
+
+    class Meta:
+        db_table = "Employments"
+        managed = False
+        unique_together = [["essn", "fid", "start_date"]]
+
+    def __str__(self):
+        return f"Employment {self.essn} at {self.fid}"
+
+    @property
+    def employee(self):
+        """Get the employee data"""
+        try:
+            return Employee.objects.get(ssn=self.essn)
+        except Employee.DoesNotExist:
+            return None
+
+    @property
+    def facility(self):
+        """Get the facility data"""
+        try:
+            return Facility.objects.get(fid=self.fid)
+        except Facility.DoesNotExist:
+            return None
+
+
+class Schedule(models.Model):
+    essn = models.IntegerField(db_column="ESSN")
+    fid = models.IntegerField(db_column="FID")
+    date = models.DateField(db_column="Date")
+    start_time = models.TimeField(db_column="StartTime")
+    end_time = models.TimeField(null=True, blank=True, db_column="EndTime")
+
+    class Meta:
+        db_table = "Schedules"
+        managed = False
+        unique_together = [["essn", "fid", "date", "start_time"]]
+
+    def __str__(self):
+        return f"Schedule {self.essn} - {self.date}"
+
+    @property
+    def employee(self):
+        """Get the employee data"""
+        try:
+            return Employee.objects.get(ssn=self.essn)
+        except Employee.DoesNotExist:
+            return None
+
+    @property
+    def facility(self):
+        """Get the facility data"""
+        try:
+            return Facility.objects.get(fid=self.fid)
+        except Facility.DoesNotExist:
+            return None
